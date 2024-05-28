d29fa0e428715b36c00838b00505d03b52c46b8f
Aashish Adhikari <155989816+adhikariaashish@users.noreply.github.com>
2024-05-28 19:30:03 +0545

    Update script.js

--- script.js
+++ script.js
@@ -41,24 +41,51 @@ function displayWeatherInfo(data) {
   card.style.display = "flex";
   const cityDisplay = document.createElement("h1");
   const tempDisplay = document.createElement("p");
+  const feelsLike = document.createElement("p");
   const humidityDisplay = document.createElement("p");
   const descDisplay = document.createElement("p");
   const weatherEmoji = document.createElement("p");
 
+  const c1 = -42.379;
+  const c2 = 2.04901523;
+  const c3 = 10.14333127;
+  const c4 = -0.22475541;
+  const c5 = -0.00683783;
+  const c6 = -0.05481717;
+  const c7 = 0.00122874;
+  const c8 = 0.00085282;
+  const c9 = -0.00000199;
+
+  const T = (temp - 273.15) * (9 / 5) + 32;
+  const HI =
+    c1 +
+    c2 * T +
+    c3 * humidity +
+    c4 * T * humidity +
+    c5 * T * T +
+    c6 * humidity * humidity +
+    c7 * T * T * humidity +
+    c8 * T * humidity * humidity +
+    c9 * T * T * humidity * humidity;
+  const HIC = (HI - 32) * (5 / 9);
+
   cityDisplay.textContent = city;
   tempDisplay.textContent = `${(temp - 273.15).toFixed(1)}°C`;
+  feelsLike.textContent = `Feels like ${HIC.toFixed(1)} °C`;
   humidityDisplay.textContent = `Humidity: ${humidity}%`;
   descDisplay.textContent = description;
   weatherEmoji.textContent = getWeatherEmoji(id);
 
   cityDisplay.classList.add("cityDisplay");
   tempDisplay.classList.add("tempDisplay");
+  feelsLike.classList.add("feelsDisplay");
   humidityDisplay.classList.add("humidityDisplay");
   descDisplay.classList.add("descDisplay");
   weatherEmoji.classList.add("weatherEmoji");
 
   card.appendChild(cityDisplay);
   card.appendChild(tempDisplay);
+  card.appendChild(feelsLike);
   card.appendChild(humidityDisplay);
   card.appendChild(descDisplay);
   card.appendChild(weatherEmoji);
