9b48769bd1a41d33085409852530840ebd778c19
Aashish Adhikari <155989816+adhikariaashish@users.noreply.github.com>
2024-05-30 17:57:14 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
 const apiKey = "1572fb81b3831efa9670dfe2d6210bfb";
 
+
 weatherForm.addEventListener("submit", async (event) => {
   event.preventDefault();
 
   const city = cityInput.value;
+
   if (city) {
     try {
       const weatherData = await getWeatherData(city);
@@ -20,31 +23,41 @@ weatherForm.addEventListener("submit", async (event) => {
   }
 });
 
+
 async function getWeatherData(city) {
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
   const response = await fetch(apiUrl);
+
 
   if (!response.ok) {
     throw new Error("Could not fetch data");
   }
   return await response.json();
 }
+
+
 function displayWeatherInfo(data) {
-  console.log(data);
+  
   const {
     name: city,
     main: { temp, humidity, feels_like },
     weather: [{ description, id }],
   } = data;
 
+
+
   card.textContent = "";
   card.style.display = "flex";
+
+
   const cityDisplay = document.createElement("h1");
   const tempDisplay = document.createElement("p");
   const feelsLike = document.createElement("p");
   const humidityDisplay = document.createElement("p");
   const descDisplay = document.createElement("p");
   const weatherEmoji = document.createElement("p");
+
+
 
   cityDisplay.textContent = city;
   tempDisplay.textContent = `${(temp - 273.15).toFixed(1)}°C`;
@@ -53,12 +66,16 @@ function displayWeatherInfo(data) {
   descDisplay.textContent = description;
   weatherEmoji.textContent = getWeatherEmoji(id);
 
+
+
   cityDisplay.classList.add("cityDisplay");
   tempDisplay.classList.add("tempDisplay");
   feelsLike.classList.add("feelsDisplay");
   humidityDisplay.classList.add("humidityDisplay");
   descDisplay.classList.add("descDisplay");
   weatherEmoji.classList.add("weatherEmoji");
+
+
 
   card.appendChild(cityDisplay);
   card.appendChild(tempDisplay);
@@ -67,6 +84,7 @@ function displayWeatherInfo(data) {
   card.appendChild(descDisplay);
   card.appendChild(weatherEmoji);
 }
+
 
 function getWeatherEmoji(weatherId) {
   switch (true) {
@@ -89,8 +107,11 @@ function getWeatherEmoji(weatherId) {
   }
 }
 
-function displayError(message) {
+
+
+  function displayError(message) {
   const errorDisplay = document.createElement("p");
+
   errorDisplay.textContent = message;
   errorDisplay.classList.add("errorDisplay");
 
@@ -98,3 +119,31 @@ function displayError(message) {
   card.style.display = "flex";
   card.appendChild(errorDisplay);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
